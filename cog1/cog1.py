26c8367c50c08120ce84c60893ad26bdd10efbe3
ShinyMemesYT <lee.elder1@gmail.com>
2017-02-26 11:45:13 +0000

    Update cog1.py

--- cog1/cog1.py
+++ cog1/cog1.py
@@ -10,6 +10,8 @@ class Mycog:
     async def punch(self, user : discord.Member):
         await self.bot.say("ONE PUNCH! And " + user.mention + " is out! ლ(ಠ益ಠლ)")
 
+    discord.on_member_join(member)
+    add_roles(member, "Members")
 
 def setup(bot):
     bot.add_cog(Mycog(bot))
